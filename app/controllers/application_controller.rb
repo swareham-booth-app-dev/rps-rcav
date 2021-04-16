4dd927f254749d7104cf7341d568e2f2eaa4153e
swareham-booth <swareham@chicagobooth.edu>
2021-04-16 19:58:44 +0000

    100%

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,20 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def homepage
+    render({:template => "game_templates/rules.html.erb"})
+  end
+
+  def play_rock
+    #redirect_to("https://www.wikipedia.org")
+    render({:template => "/game_templates/user_rock.html.erb"})
+  end
+  def play_paper
+    #redirect_to("https://www.wikipedia.org")
+    render({:template => "/game_templates/user_paper.html.erb"})
+  end
+  def play_scissors 
+    render({:template => "/game_templates/user_scissors.html.erb"})
+  end
+
 end
